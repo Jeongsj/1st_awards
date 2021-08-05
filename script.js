937df301ebf56fff22ef23e934b1afd079c651bb
JeongSJ <37651774+Jeongsj@users.noreply.github.com>
2021-08-05 14:01:41 +0900

    Add link

--- script.js
+++ script.js
@@ -44,21 +44,21 @@ $(document).ready(function() {
 $.getJSON('hair.json', function(items){
     items.sort(() => Math.random() - 0.5);
     $.each(items, function(i, item){
-        $("#hair").append(`<div class="item"><img class="ui image" src="./proc/${item.i}.png"><div class="meta"><a href="https://maplestory.nexon.com/Promotion/2021/20210805/AwardsPoll/Out/1/${item.i}/141" target="_blank">${item.name}</a></div> </div>`);
+        $("#hair").append(`<div class="item"><img class="ui image" src="./proc/${item.i}.png"><div class="meta"><a href="https://maplestory.nexon.com/Promotion/2021/20210805/AwardsPoll/Out/1/${item.i}/141" target="_blank">${item.name}</a></div><div class="description">${item.user}</div> </div>`);
     });
 });
 $.getJSON('face.json', function(items){
     items.sort(() => Math.random() - 0.5);
     $.each(items, function(i, item){
-        $("#face").append(`<div class="item"><img class="ui image" src="./proc4/${item.i}.png"><div class="meta"><a href="https://maplestory.nexon.com/Promotion/2021/20210805/AwardsPoll/Out/2/${item.i}/141" target="_blank">${item.name}</a></div> </div>`);    });
+        $("#face").append(`<div class="item"><img class="ui image" src="./proc4/${item.i}.png"><div class="meta"><a href="https://maplestory.nexon.com/Promotion/2021/20210805/AwardsPoll/Out/2/${item.i}/141" target="_blank">${item.name}</a></div><div class="description">${item.user}</div> </div>`);    });
 });
 $.getJSON('clothes.json', function(items){
     items.sort(() => Math.random() - 0.5);
     $.each(items, function(i, item){
-        $("#clothes").append(`<div class="item"><img class="ui image" src="./proc2/${item.i}.png"><div class="meta"><a href="https://maplestory.nexon.com/Promotion/2021/20210805/AwardsPoll/Out/3/${item.i}/141" target="_blank">${item.name}</a></div> </div>`);    });
+        $("#clothes").append(`<div class="item"><img class="ui image" src="./proc2/${item.i}.png"><div class="meta"><a href="https://maplestory.nexon.com/Promotion/2021/20210805/AwardsPoll/Out/3/${item.i}/141" target="_blank">${item.name}</a></div><div class="description">${item.user}</div> </div>`);    });
 });
 $.getJSON('pet.json', function(items){
     items.sort(() => Math.random() - 0.5);
     $.each(items, function(i, item){
-        $("#pet").append(`<div class="item"><img class="ui image" src="./proc3/${item.i}.png"><div class="meta"><a href="https://maplestory.nexon.com/Promotion/2021/20210805/AwardsPoll/Out/4/${item.i}/141" target="_blank">${item.name}</a></div> </div>`);    });
+        $("#pet").append(`<div class="item"><img class="ui image" src="./proc3/${item.i}.png"><div class="meta"><a href="https://maplestory.nexon.com/Promotion/2021/20210805/AwardsPoll/Out/4/${item.i}/141" target="_blank">${item.name}</a></div><div class="description">${item.user}</div> </div>`);    });
 });
